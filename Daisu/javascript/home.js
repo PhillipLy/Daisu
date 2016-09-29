156890c60f02a7a22a4f6ebdbe9a905d90cf2442
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-09-28 22:47:27 -0700

    make login button work to display login form

---
+++ Daisu/javascript/home.js
@@ -0,0 +1,30 @@
+var main = function() {
+    //display login form
+    var $formSlideDown = function () {
+        $('#login-form').transition('slide down');
+    };
+
+    // active or disable dimmer
+    var $dimpage = function() {
+        $('.ui.page.dimmer').toggleClass('disabled active');
+    };
+
+    //click login-button
+    $('#login-button').on('click', function() {
+        $dimpage();
+        $formSlideDown();
+    });
+
+    //chick login form
+    $('#login-form').click(function(event) {
+        //check if click outside the form
+        if($(event.target).is('#login-form')) {
+            $formSlideDown();
+            setTimeout(function() {
+                $dimpage();
+            }, 200);
+        } 
+
+    });
+};
+$(document).ready(main);
