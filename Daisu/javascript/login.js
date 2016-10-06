a55b630b945ad3996ae8d50398403224a517f1b0
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-10-05 23:52:37 -0700

    change the login form into a new page

--- Daisu/javascript/login.js
+++ Daisu/javascript/login.js
@@ -1,33 +1,6 @@
 var main = function() {
     'use strict';
     
-    //display login form
-    var $formSlideDown = function () {
-        $('#login-form').transition('slide down');
-    };
-
-    // active or disable dimmer for login form
-    var $dimpage = function() {
-        $('.ui.page.dimmer').toggleClass('disabled active');
-    };
-
-    //click menu-login-button
-    $('#menu-login-button').on('click', function() {
-        $dimpage();
-        $formSlideDown();
-    });
-
-    //chick login form
-    $('#login-form').click(function(event) {
-        //check if click outside the form
-        if($(event.target).is('#login-form')) {
-            $formSlideDown();
-            setTimeout(function() {
-                $dimpage();
-            }, 200);
-        } 
-    });
-
     //login button pressed
     $('#login-button').on('click', function() {
         var username = $('#username').val(),
