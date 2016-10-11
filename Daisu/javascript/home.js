d9695348c9af605bb823c46400ad7c0aee385287
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-10-11 13:37:27 -0700

    create user account menu bar

---
+++ Daisu/javascript/home.js
@@ -0,0 +1,13 @@
+var main = function () {
+    'use strict';
+    
+    $('.ui.pointing.dropdown').dropdown({
+        on: 'hover'
+    });
+
+/*
+    $('.ui.right.pointing.dropdown').on('click', function () {
+        $('.ui.right.pointing.dropdown').addClass("active visible");
+    });
+*/
+};
