9b99d860787070e5d2d6a744dfac521965549b73
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-11-02 19:41:42 -0700

    change index page into html and check login by call check-login.php

--- Daisu/javascript/home.js
+++ Daisu/javascript/home.js
@@ -1,12 +1,28 @@
 var main = function () {
     'use strict';
     
+    //enable user-menu dropdown
     $('.ui.pointing.dropdown').dropdown({
         on: 'click',
         transition: 'fade down',
 		action: 'nothing'
     });
 
+    //check if user already logged in
+    $.ajax({
+    	url:'./php/check-login.php',
+        method:'POST',
+        data:{username:username, password:password},
+        cache: false,
+        success:function(data) {
+            if(data) {
+                $('.right.menu').append(data);
+            }
+            else {
+                console.log("cannot load check-login.php");
+            }
+        }
+    });
 /*
     $('.ui.right.pointing.dropdown').on('click', function () {
         $('.ui.right.pointing.dropdown').addClass("active visible");
