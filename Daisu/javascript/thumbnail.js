39654bdb8ec39321701022f556faff9a9cc99746
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-11-09 02:43:26 -0800

    create product list in thumbnail and minor change on menu dropdown

--- Daisu/javascript/thumbnail.js
+++ Daisu/javascript/thumbnail.js
@@ -24,8 +24,57 @@ var main = function() {
         history : true
     });
 
-    
+    /*
+    //card frame for product item
+    var generateCard = function (url, title, description, star, review) {
+        var card = '<div class="ui card" style="display: none;">'+
+                        '<div class="blurring dimmable image">'+
+                            '<div class="ui dimmer">' +
+                                '<div class="content">' +
+                                    '<div class="center">' +
+                                        '<div class="ui inverted button">Get Detail</div>' +
+                            '</div></div></div>' +
+                            '<img src="' + url + '">'+
+                        '</div>'+
+                        '<div class="content">' +
+                            '<a class="header">' + title + '</a>' +
+                            '<div class="description">' + description + '</div>'+
+                        '</div>'+
+                        '<div class="extra aligned">'+
+                            '<div class="right floated">' + review + ' Review</div>' +
+                            '<div class="ui star rating" data-rating="' + star + '" data-max-rating="5">'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>';
 
+        return card;
+    };
+
+    //add 
+    var addStackCards = function (data) {
+        var $columnCard = $('<div class="doubling stackable four column ui four cards">');
+        var i = 0;
+        //
+        var card = generateCard(data.url, data.title, data.description, data.star, data.review);
+
+        for(;i<8;i++) {
+            $columnCard.append(card);
+        }
+    };
+
+    var displayCards = function() {
+        var data = {"url":"http://semantic-ui.com/examples/assets/images/wireframe/image.png",
+                    "title":"Custome",
+                    "price": 1.00,
+                    "description":"Products Gear Description",
+                    "star":4,
+                    "review":2};
+        
+        addStackCards(data);
+    };
+
+    displayCards();
+    */
 };
 
 $(document).ready(main);
