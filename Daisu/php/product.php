5cfc711d597eedca7a376b0f397e6da2cf42648e
asangar94 <asangar94@gmail.com>
2016-11-17 15:04:51 -0800

    Add files via upload

---
+++ Daisu/php/product.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+
+    //datavase connection
+    $connect = mysqli_connect("localhost", "root", "", "daisu_db");
+    
+    //check if username and password have values
+    if(isset($_GET["itemId"])) {
+        $itemid = mysqli_real_escape_string($connect, $_POST["itemId"]);
+
+        //database query request
+        $sql = "SELECT * FROM detail WHERE itemid = '" . $itemid . "'";
+       
+        $result = mysqli_query($connect, $sql);
+
+        $num_row = mysqli_num_rows($result);
+        
+        $description = [];
+
+        if($num_row > 0) {
+            while($data = mysqli_fetch_array($result)) {
+               $arr = ('detail' => $data["detail"]);
+                
+               array_push(description, json_encode($arr));
+            }
+            echo description;
+        }
+    }
+?>
---
+++ Daisu/php/thumbnail.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+
+    //datavase connection
+    $connect = mysqli_connect("localhost", "root", "", "daisu_db");
+    //check if username and password have values
+    if(isset($_GET["category"])) {
+        $category = mysqli_real_escape_string($connect, $_GET["category"]);
+
+        //database query request
+        $sql = "SELECT * FROM `item` WHERE category = '" . $category . "'";
+       
+        $result = mysqli_query($connect, $sql);
+
+        $num_row = mysqli_num_rows($result);
+        
+        $item = [];
+
+        if($num_row > 0) {
+            while($data = mysqli_fetch_array($result)) {
+               $arr = ('itemId' => $data["itemid"], 'title' => $data["productname"], 'brand' => $data["brand"],
+                 'price' => $data["price"], 'color' => $data["color"], 'url' => $data["picturelink"];
+                
+               array_push(item, json_encode($arr));
+            }
+            echo item;
+        }
+    }
+?>
